7752186de96782ba3a2032ff2a14adc84e5a6de7
vinay kumar <vinnuborra3@gmaie.com>
2023-06-10 14:45:33 +0530

    Update

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 Deploy Address(LINEA-TESTNET);
 
-RewardToken contract deployed to: 0x9016d3DCc8C4DbA4f5778581236F9eCC8D0026cF
-Staking contract deployed to: 0x926812D6136f84Bc35bB772A140de1942aB354B6
+RewardToken contract deployed to: 0xf48B2086a41B9eF1C5A188d4eE8cb92c8F542F48
+
+Staking contract deployed to: 0xeE3b4d1b118875E6a514972d2a5708F3F693Bcbb
+
 MetaMask: 0x3fAeBd4a2C1B1CaDdcb99CA7dC5381d5683122Ba
 
 <!-- This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
--- src/components/Stake.js
+++ src/components/Stake.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { ethers, utils } from "ethers";
-import RewardToken from "../contracts/RewardToken.json";
-import Staking from "../contracts/Staking.json";
+import React, { useState, useEffect } from 'react';
+import { ethers, utils } from 'ethers';
+import Staking from '../contracts/Staking.json';
+import RewardToken from '../contracts/RewardToken.json';
 
 const Stake = () => {
-  const [rewardTokenContract, setRewardTokenContract] = useState(null);
   const [stakingContract, setStakingContract] = useState(null);
-  const [account, setAccount] = useState("");
-  const [stakedAmount, setStakedAmount] = useState(0);
+  const [rewardTokenContract, setRewardTokenContract] = useState(null);
+  const [account, setAccount] = useState('');
+  const [stakedAmount, setStakedAmount] = useState('');
   const [rewardBalance, setRewardBalance] = useState(0);
   const [selectedAmount, setSelectedAmount] = useState(0);
   const [error, setError] = useState(null);
-  const [tokenDecimals, setTokenDecimals] = useState(0);
 
   useEffect(() => {
     connect();
@@ -21,40 +20,33 @@ const Stake = () => {
     try {
       if (window.ethereum) {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
-        await window.ethereum.request({ method: "eth_requestAccounts" });
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
 
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         setAccount(address);
 
-        const rewardTokenAddress = "0xf48B2086a41B9eF1C5A188d4eE8cb92c8F542F48";
-        const stakingContractAddress = "0xeE3b4d1b118875E6a514972d2a5708F3F693Bcbb";
+        const stakingContractAddress = '0x9D8f7dF6c746BD5b0ba3e916F7604503c03DC174';
+        const rewardTokenContractAddress = '0xc0C76c75545836db4461CbA428190cB630Aa57A7';
 
-        const rewardTokenContract = new ethers.Contract(
-          rewardTokenAddress,
-          RewardToken.abi,
-          signer
-        );
         const stakingContract = new ethers.Contract(
           stakingContractAddress,
           Staking.abi,
           signer
         );
+        const rewardTokenContract = new ethers.Contract(
+          rewardTokenContractAddress,
+          RewardToken.abi,
+          signer
+        );
 
-        setRewardTokenContract(rewardTokenContract);
         setStakingContract(stakingContract);
+        setRewardTokenContract(rewardTokenContract);
 
         const stakedAmount = await stakingContract.getStaked(address);
-        setStakedAmount(stakedAmount.toNumber());
-
-        const rewardBalance = await rewardTokenContract.balanceOf(address);
-        setRewardBalance(rewardBalance.toNumber());
-
-        // Fetch the token decimals
-        const tokenDecimals = await rewardTokenContract.decimals();
-        setTokenDecimals(tokenDecimals.toNumber());
+        setStakedAmount(stakedAmount.toString() || '');
       } else {
-        setError("Metamask is not installed or not detected.");
+        setError('Metamask is not installed or not detected.');
       }
     } catch (error) {
       setError(error.message);
@@ -62,38 +54,57 @@ const Stake = () => {
   };
 
   const disconnect = () => {
-    setRewardTokenContract(null);
     setStakingContract(null);
-    setAccount("");
-    setStakedAmount(0);
-    setRewardBalance(0);
+    setRewardTokenContract(null);
+    setAccount('');
+    setStakedAmount('');
     setSelectedAmount(0);
     setError(null);
   };
 
   const stakeTokens = async () => {
     try {
-      const tokenAmount = utils.parseUnits(selectedAmount.toString(), tokenDecimals);
+      if (selectedAmount <= 0) {
+        setError('Please enter a valid token amount.');
+        return;
+      }
 
-      // Approve the staking contract to spend tokens
-      const approveTx = await rewardTokenContract.approve(stakingContract.address, tokenAmount);
-      await approveTx.wait();
+      const tokenAmount = utils.parseUnits(selectedAmount.toString());
+
+      // Approve staking contract to spend tokens
+      await rewardTokenContract.approve(stakingContract.address, tokenAmount, { gasLimit: 500000 });
 
       // Stake tokens
-      await stakingContract.stake(tokenAmount);
-      setStakedAmount(stakedAmount + tokenAmount.toNumber());
+      await stakingContract.stake(tokenAmount, { gasLimit: 500000 });
+
+      // Fetch updated staked amount
+      const updatedStakedAmount = await stakingContract.getStaked(account);
+      setStakedAmount(updatedStakedAmount.toString() || '');
+
+      // Fetch updated reward balance
+      const updatedRewardBalance = await stakingContract.earned(account);
+      setRewardBalance(updatedRewardBalance.toString());
     } catch (error) {
       setError(error.message);
     }
   };
 
-  const unstakeTokens = async () => {
-    try {
-      const tokenAmount = utils.parseUnits(selectedAmount.toString(), tokenDecimals);
 
-      // Unstake tokens
-      await stakingContract.withdraw(tokenAmount);
-      setStakedAmount(stakedAmount - tokenAmount.toNumber());
+  const withdrawTokens = async () => {
+    try {
+      if (selectedAmount <= 0) {
+        setError('Please enter a valid token amount.');
+        return;
+      }
+
+      const tokenAmount = utils.parseUnits(selectedAmount.toString());
+
+      // Withdraw tokens
+      await stakingContract.withdraw(tokenAmount, { gasLimit: 500000 });
+
+      // Update the staked amount
+      const updatedStakedAmount = await stakingContract.getStaked(account);
+      setStakedAmount(updatedStakedAmount.toString() || '');
     } catch (error) {
       setError(error.message);
     }
@@ -101,79 +112,80 @@ const Stake = () => {
 
   const claimRewards = async () => {
     try {
-      // Calculate the earned rewards based on the staked amount
-      const earnedRewards = await stakingContract.calculateEarnedRewards(account);
+      // Claim rewards
+      await stakingContract.claimReward({ gasLimit: 500000 });
 
-      // Only allow claiming rewards if there are earned rewards
-      if (earnedRewards.gt(0)) {
-        // Claim rewards
-        const claimTx = await stakingContract.claimReward();
-        await claimTx.wait();
-
-        const rewardBalance = await rewardTokenContract.balanceOf(account);
-        setRewardBalance(rewardBalance.toNumber());
-      } else {
-        setError("No rewards available to claim.");
-      }
+      // Update the reward balance
+      const rewardBalance = await stakingContract.earned(account);
+      setRewardBalance(rewardBalance.toString());
     } catch (error) {
       setError(error.message);
     }
   };
 
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">Staking App</h1>
-      {account ? (
-        <div>
-          <p>Connected Account: {account}</p>
-          <p>Staked Amount: {stakedAmount} tokens</p>
-          <p>Reward Balance: {rewardBalance} tokens</p>
-          <div className="flex items-center mt-4">
-            <input
-              type="number"
-              min="0"
-              value={selectedAmount}
-              onChange={(e) => setSelectedAmount(e.target.value)}
-              className="border border-gray-300 px-4 py-2 rounded mr-4"
-            />
-            <button
-              onClick={stakeTokens}
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-            >
-              Stake
-            </button>
-            <button
-              onClick={unstakeTokens}
-              className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded ml-4"
-            >
-              Unstake
-            </button>
-            <button
-              onClick={claimRewards}
-              className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded ml-4"
-            >
-              Claim Rewards
-            </button>
-            <button
-              onClick={disconnect}
-              className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded ml-4"
-            >
-              Disconnect Wallet
-            </button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-cyan-800">
+      <div className="container mx-auto p-4 bg-blue-100 shadow-md rounded-md ">
+        <h1 className="text-4xl font-bold text-center mb-4">Staking Dapp</h1>
+        {account ? (
+          <div>
+            <p className="text-center">Connected Account: {account}</p>
+            {stakedAmount !== '' && (
+              <p className="text-center">
+                Staked Amount: {utils.formatUnits(stakedAmount, 'ether')} tokens
+              </p>
+            )}
+            <p className="text-center">Reward Balance: {rewardBalance} tokens</p>
+            <div className="mt-4 flex justify-center">
+              <label htmlFor="amount" className="mr-2">
+                Enter Token Amount:
+              </label>
+              <input
+                type="number"
+                id="amount"
+                value={selectedAmount}
+                onChange={(e) => setSelectedAmount(e.target.value)}
+                className="border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div className="mt-4 flex justify-center">
+              <button
+                onClick={stakeTokens}
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+              >
+                Stake Tokens
+              </button>
+              <button
+                onClick={withdrawTokens}
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2"
+              >
+                Withdraw Tokens
+              </button>
+              <button
+                onClick={claimRewards}
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2"
+              >
+                Claim Rewards
+              </button>
+              <button
+                onClick={disconnect}
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded ml-2"
+              >
+                Disconnect
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div>
-          <p>Connect your wallet to get started.</p>
+        ) : (
           <button
             onClick={connect}
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-4"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
           >
-            Connect Wallet
+            Connect with Metamask
           </button>
-        </div>
-      )}
-      {error && <p className="text-red-500 mt-4">{error}</p>}
+        )}
+        {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
+      </div>
     </div>
   );
 };
